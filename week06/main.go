658d9699de1e1c099cbc56cde94b39b9a0d8180f
전홍준 <jhongj0524@gmail.com>
2025-09-29 12:13:43 +0900

    0.3v go

--- week06/main.go
+++ week06/main.go
@@ -6,14 +6,13 @@ import (
 )
 
 func main() {
-	//var name string
-	// name = "Jeon Hongjun"
+	var f64 float64
+	var str string
+	var i32 int32
+	var b bool
 
-	//var name = "Jeon Hongjun"
-
-	//name := "Jeon Hongjun"
-
-	name := 2.71
-
-	fmt.Println(name, reflect.TypeOf(name))
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(str, reflect.TypeOf(str))
+	fmt.Println(i32, reflect.TypeOf(i32))
+	fmt.Println(b, reflect.TypeOf(b))
 }
